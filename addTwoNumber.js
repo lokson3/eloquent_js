d63233f92b3406e3da1562ad5690f515aa2e0ddf
Oloko Olabisi <olokoolabisi.n@gmail.com>
2024-07-09 15:54:17 +0100

    first algorithm commit

---
+++ addTwoNumber.js
@@ -0,0 +1,5 @@
+function addTwoNums(a, b) {
+    return a + b;
+}
+
+console.log(addTwoNums(8, 5))
---
+++ primeNumber.js
@@ -0,0 +1,9 @@
+function isPrime(n) {
+    if(n % 2 != 0) {
+        return true
+    }else {
+        return false
+    }
+}
+console.log(isPrime(5))
+console.log(isPrime(4))
